f1ad2a9cddfa645e0d8da6b5cd6ff6b203c95d84
omaramin-2000 <omaramin2000@hotmail.com>
2025-12-04 18:50:08 +0200

    Add librosa module to requirements

--- setup.py
+++ setup.py
@@ -5,9 +5,9 @@ setup(
     version="0.1.0",
     packages=find_namespace_packages(include=['custom_components.*']),
     install_requires=[
-        'voluptuous>=0.13.1',
-        'homeassistant>=2024.3.3',
-        'aiofiles>=23.2.1',
-        'librosa>=0.10.0',
+        'voluptuous',
+        'homeassistant',
+        'aiofiles',
+        'librosa',
     ],
 )

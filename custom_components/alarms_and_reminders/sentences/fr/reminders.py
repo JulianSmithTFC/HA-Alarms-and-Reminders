a2b46b4e3830a773d84b45781cb4a9f2f2c3c995
Omar Amin <65174210+omaramin-2000@users.noreply.github.com>
2025-12-17 00:30:45 +0200

    Create reminders.py

---
+++ custom_components/alarms_and_reminders/sentences/fr/reminders.py
@@ -0,0 +1,47 @@
+DEFAULT_SENTENCES = {
+    "language": "de",
+    "intents": {
+        "SetReminder": {
+            "data": [
+                {
+                    "sentences": [
+                        "erinnere mich an {task} um {datetime}",
+                        "[neue] Erinnerung an {task} um {datetime}",
+                        "erstelle eine Erinnerung an {task} um {datetime}",
+                    ]
+                }
+            ]
+        },
+        "StopReminder": {
+            "data": [
+                {
+                    "sentences": [
+                        "Erinnerung (stoppen|löschen|beenden|abbrechen|ausschalten)",
+                    ]
+                }
+            ]
+        },
+        "SnoozeReminder": {
+            "data": [
+                {
+                    "sentences": [
+                        "pausiere [die] Erinnerung",
+                        "erinnere mich später [nochmal]",
+                        "pausiere die Erinnerung [für] {minutes} Minuten",
+                        "erinnere mich [nochmal] in {minutes} Minuten",
+                    ]
+                }
+            ]
+        }
+    },
+    "lists": {
+        # ...existing code for task, datetime, time, etc...
+        "minutes": {
+            "type": "number",
+            "range": {
+                "from": 1,
+                "to": 60
+            }
+        }
+    }
+}

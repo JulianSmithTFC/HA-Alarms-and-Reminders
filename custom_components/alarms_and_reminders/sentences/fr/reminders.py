c5ee6b9da2f10df7961c84ae3b8679d9e39daf1d
Omar Amin <omaramin2000@hotmail.com>
2025-12-17 11:51:39 +0200

    Correct reminder sentences

--- custom_components/alarms_and_reminders/sentences/fr/reminders.py
+++ custom_components/alarms_and_reminders/sentences/fr/reminders.py
@@ -3,97 +3,39 @@
 DEFAULT_SENTENCES = {
     "language": "fr",
     "intents": {
-        "SetAlarm": {
+        "SetReminder": {
             "data": [
                 {
                     "sentences": [
-                        "mets une alarme [pour|à] {datetime}",
-                        "réveille moi à {datetime}",
+                        "rappelle moi de {task} à {datetime}",
+                        "mets un rappel de {task} à {datetime}",
                     ]
                 }
             ]
         },
-        "StopAlarm": {
+        "StopReminder": {
             "data": [
                 {
                     "sentences": [
-                        "arrête l'alarme",
-                        "éteins l'alarme",
-                        "désactive l'alarme",
-                        "annule l'alarme",
+                        "annules le rappel",
+                        "enlèves le rappel",
+                        "désactives le rappel",
                     ]
                 }
             ]
         },
-        "SnoozeAlarm": {
+        "SnoozeReminder": {
             "data": [
                 {
                     "sentences": [
-                        "repousse l'alarme",
-                        "repousse l'alarme de {minutes} minutes",
-                        "donne-moi encore {minutes} minutes",
+                        "repousses le rappel",
+                        "repousses le rappel de {minutes} minutes",
                     ]
                 }
             ]
         }
     },
-    "lists": {
-        "datetime": {
-            "type": "text",
-            "values": [
-                "vers {time}",
-                "à {time}",
-                "{time} le {date}",
-                "aujourd'hui à {time}",
-                "demain à {time}",
-                "après-demain à {time}",
-                "le {date} à {time}",
-            ]
-        },
-        "time": {
-            "type": "text",
-            "values": [
-                "{hour} heures {minute}",
-                "{hour} heures",
-                "{hour} heures du matin",
-                "{hour} heures de l'après-midi",
-                "{hour} heures du soir",
-            ]
-        },
-        "hour": {
-            "type": "number",
-            "range": {
-                "from": 1,
-                "to": 23
-            }
-        },
-        "minute": {
-            "type": "number",
-            "range": {
-                "from": 0,
-                "to": 59,
-                "step": 1
-            }
-        },
-        "date": {
-            "type": "text",
-            "values": [
-                "Lundi",
-                "Mardi",
-                "Mercredi",
-                "Jeudi",
-                "Vendredi",
-                "Samedi",
-                "Dimanche",
-                "Lundi prochain",
-                "Mardi prochain",
-                "Mercredi prochain",
-                "Jeudi prochain",
-                "Vendredi prochain",
-                "Samedi prochain",
-                "Dimanche prochain",
-            ]
-        },
+    "lists": {    
         "minutes": {
             "type": "number",
             "range": {

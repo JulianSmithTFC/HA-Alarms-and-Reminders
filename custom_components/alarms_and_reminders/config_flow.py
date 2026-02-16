5442efcc8b3e5ae4a374b9989f9101f13d2d14a3
Julian <jsmith@wisperisp.com>
2026-02-15 21:34:11 -0600

    test

--- custom_components/alarms_and_reminders/config_flow.py
+++ custom_components/alarms_and_reminders/config_flow.py
@@ -21,16 +21,11 @@ class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
         return self.async_show_form(step_id="user", data_schema=vol.Schema({}))
 
     @staticmethod
-    @callback
     def async_get_options_flow(config_entry: config_entries.ConfigEntry):
         return OptionsFlowHandler(config_entry)
 
 
 class OptionsFlowHandler(config_entries.OptionsFlow):
-    """Options flow to set default satellite."""
-    def __init__(self, config_entry: config_entries.ConfigEntry) -> None:
-        self.config_entry = config_entry
-
     async def async_step_init(self, user_input=None):
         if user_input is not None:
             return self.async_create_entry(title="", data=user_input)
@@ -46,3 +41,5 @@ class OptionsFlowHandler(config_entries.OptionsFlow):
         )
 
         return self.async_show_form(step_id="init", data_schema=schema)
+
+
